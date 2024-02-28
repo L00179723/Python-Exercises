09fe7599da0ca20aa67ae008607044feb15d0499
Suzanne Freitas <158096342+L00179723@users.noreply.github.com>
2024-02-28 20:59:55 +0000

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,8 @@
+__author__ = "Suzanne Freitas"
+__contact__ = "l00179723"
+__copyright__ = "Copyright $YEAR, JOR"
+__date__ = "12FEB23"
+__deprecated__ = False
+__license__ = "GPLv3"
+__status__ = "Production"
+__version__ = "1.2.1"
